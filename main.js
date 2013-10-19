59322a6564b221320de5ef0fe371ca840f47d588
copperrose <c200519348@hotmail.com>
2013-10-19 15:08:57 +0800

    fix #8

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-(function (){
-var canvas = document.getElementById('game');
-var ctx = canvas.getContext('2d');
-canvas.width = 480;
-canvas.height = 320;
+var gameModule=(function (){
+	function start(){
 
-var balla=Math.floor(Math.random()*300);//0~300
-var ballb=Math.floor(Math.random()*500);
-var ballc=Math.floor(Math.random()*100);
+		var canvas = document.getElementById('game');
+		var ctx = canvas.getContext('2d');
+		canvas.width = 480;
+		canvas.height = 320;
 
-ctx.fillStyle='black';
-ctx.beginPath();
-ctx.arc(balla, ballb, ballc, 0, Math.PI * 2 , true);
-ctx.fill();
+		var balla=Math.floor(Math.random()*300);//0~300
+		var ballb=Math.floor(Math.random()*500);
+		var ballc=Math.floor(Math.random()*100);
+
+		ctx.fillStyle='black';
+		ctx.beginPath();
+		ctx.arc(balla, ballb, ballc, 0, Math.PI * 2 , true);
+		ctx.fill();
+	}
+
+	return{
+		start:start
+	}
 }) ();
+
+gameModule.start();
