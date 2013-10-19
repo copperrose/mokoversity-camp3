f281b0fcc23d2c41f040cb7c5aa234648237fa39
copperrose <c200519348@hotmail.com>
2013-10-19 14:38:50 +0800

    fix #7

---
+++ main.js
@@ -0,0 +1,15 @@
+(function (){
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+canvas.width = 480;
+canvas.height = 320;
+
+var balla=Math.floor(Math.random()*300);//0~300
+var ballb=Math.floor(Math.random()*500);
+var ballc=Math.floor(Math.random()*100);
+
+ctx.fillStyle='black';
+ctx.beginPath();
+ctx.arc(balla, ballb, ballc, 0, Math.PI * 2 , true);
+ctx.fill();
+}) ();
