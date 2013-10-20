57e8a93e24b0e1f566d920cf94ad51bce8aea8eb
copperrose <c200519348@hotmail.com>
2013-10-20 10:59:33 +0800

    fix #12

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 var gameModule=(function (){
-	var counter=0,timeoutVar;
-	/*var canvas = document.getElementById('game');
-	var ctx = canvas.getContext('2d');
-	canvas.width = 480;
-	canvas.height = 320;
-	*/
+	var counter=0,
+		scores=0,
+		timeoutVar,
+		balla,ballb,ballc;
+	//	length=color.length;
 
 	function touchEvent(evt){
 		var x=evt.clientX,
-			y=evt.clientY;
+			y=evt.clientY,
+			tmp=(balla-x)*(balla-x)+(ballb-y)*(ballb-y);
 
 			console.log("Clicked:"+x+","+y);
+
+			if(tmp<ballc*ballc){
+				console.log("Hit!Your Scores:"+scores);
+			}
 	}
 	function start(){
 		document.getElementById("main").addEventListener("click",touchEvent,false);
@@ -21,25 +25,29 @@ var gameModule=(function (){
 
 		var canvas = document.getElementById('game');
 		var ctx = canvas.getContext('2d');
-		canvas.width = 480;
-		canvas.height = 320;
+		canvas.width = 640;
+		canvas.height = 480;
 
-		var balla=Math.floor(Math.random()*300);//0~300
-		var ballb=Math.floor(Math.random()*500);
-		var ballc=Math.floor(Math.random()*100);
+		balla=Math.floor(Math.random()*300);//0~300
+		ballb=Math.floor(Math.random()*500);
+		ballc=Math.floor(Math.random()*100);
 
 		ctx.fillStyle='black';
 		ctx.beginPath();
 		ctx.arc(balla, ballb, ballc, 0, Math.PI * 2 , true);
 		ctx.fill();
 
-		if(counter>=100){
-
+		if(counter>=20){
+			gameOver();
 		}else{
 			timeoutVar=setTimeout(start,1000);//1sec
 			counter=counter+1;
 			console.log("counter="+counter);
 		}
+	}
+
+	function gameOver(){
+
 	}
 
 	return{
