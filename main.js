184e527ea5bb2702ece52bebfad5b9847fe88a56
copperrose <c200519348@hotmail.com>
2013-10-20 10:39:48 +0800

    fix #11

--- main.js
+++ main.js
@@ -6,7 +6,18 @@ var gameModule=(function (){
 	canvas.height = 320;
 	*/
 
+	function touchEvent(evt){
+		var x=evt.clientX,
+			y=evt.clientY;
+
+			console.log("Clicked:"+x+","+y);
+	}
 	function start(){
+		document.getElementById("main").addEventListener("click",touchEvent,false);
+		startGame();
+	}
+
+	function startGame(){
 
 		var canvas = document.getElementById('game');
 		var ctx = canvas.getContext('2d');
